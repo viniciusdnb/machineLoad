1882c2dbcd851698e802c084833819cdf8aa3cca
Vinicius Henrique costa da silva <71468843+viniciusdnb@users.noreply.github.com>
2025-05-08 19:52:26 -0300

    Update structureCore.js
    Update and general rebilling of the code structure

--- Libs/machineLoad/Core.js
+++ Libs/machineLoad/Core.js
@@ -1,112 +1,92 @@
-
-    
 class Core {
-
-    constructor(configWorks, prevision) {
-        this.configWorks = configWorks;
-        this.prevision = prevision;
+    constructor(config) {
+        this.config = config;
     }
 
-    nextDate() {
+    setStartWork(dateTime, timeResidual = 0) {
+        var intTimeStart = parseInt(this.config.timeStart);
+        var intTimeResidual = parseInt(timeResidual)
+        var newDateTime = dateTime.setHours(intTimeStart + intTimeResidual);
 
-        var dateTimeNow = new Date(this.prevision);
-        var timeLimit = this.configWorks.timeLimit;
-        dateTimeNow.setHours(timeLimit);
-        var previsionHour = this.prevision.getHours();
-        var hourLimit = dateTimeNow.getHours();
+        return new Date(newDateTime);
+    }
 
-        //verifica se a previsao esta dentro do horario permitido ate as 17
-        //caso esteja fora acresentara mais um dia na data 
-        //e verifica se a nova data é dia util
-        var result =  previsionHour - hourLimit;
-        if(result <= -7){
-            
-            for (let i = 0; i <= 7; i++) {
-                if(previsionHour == i){
-                    var hourResidual = 7 + i
-                }   
+    getIntTimeResidualAfterLimit(dateTime) {
+       //FUNCAO QUE PEGA O HORARIO RESIDUAL APOS AS 17 ATÉ AS 00 HORAS
+        var intTimeLimit = parseInt(this.config.timeLimit);
+        var newTimeResidual = "";
+        if (dateTime.getHours() > intTimeLimit || dateTime.getHours() == 0) {
+            if (dateTime.getHours() == 0) {
+                newTimeResidual = 7;
+            } else {
+                newTimeResidual = dateTime.getHours() - intTimeLimit;
             }
+
+            return parseInt(newTimeResidual);
+        } else {
+            return dateTime
+        }
+    }
+
+    verifyDateTimeAfterLimit(dateTime){
+        var intDateTime = dateTime.getHours();
+        
+        if(intDateTime >= parseInt(this.config.timeLimit))
+        {   
+            return {
+                "newDateTimeAfter": true,
+                "dateTime":new Date(dateTime.setDate(dateTime.getDate()+1))
+            };
         }else{
-            var hourResidual = previsionHour - hourLimit
+            return {
+                "newDateTimeAfter": false,
+                "dateTime": new Date(dateTime)
+            }
         }
-
-        
-        
-
-        if (previsionHour >= hourLimit) {
-            var nextDay = dateTimeNow.getDate() + 1;
-            
-           
-            var newDateTime = this.setNewtimeDay(dateTimeNow, nextDay, hourResidual);
-            return { "newPrevision": this.verifyDateWeekend(newDateTime,hourResidual)};
-            //mesmo que a previsao esteja dentro do permitido verifca se é dia util
-        } else if (dateTimeNow.getDay() == 0 || dateTimeNow.getDay() == 6) {
-            return { "newPrevision": this.verifyDateWeekend(dateTimeNow,hourResidual)};
-        }
-        //caso a previsao esteja dentro do horario permito verifica se 
-        //a previsao nao esta de madrugada
-        //caso esteja de madrugada set uma novo horario de inicio do trabalho
-        return { "newPrevision": this.verifyDateWeekend(this.nextHourStart(),hourResidual) };
-
     }
 
-    nextHourStart() {
-        //verifica se o horario nao esta de madrugada e acerta para a primeira hora de trabalho
-        var dateTimeNow = new Date(this.prevision);
-        var timeStart = this.configWorks.timeStart;
-        dateTimeNow.setHours(timeStart);
-        var previsionHour = this.prevision.getHours();
-        var hourStart = dateTimeNow.getHours();
-
-        if (previsionHour < hourStart) {
-            return this.setNewtimeDay(dateTimeNow, dateTimeNow.getDate(), 0);
-        }
-
-        return this.prevision;
-    }
-
-    verifyDateWeekend(dateTimeNow, hourResidual) {
-        //sabado > numero 6
-        //domingo > numero 0
-        //console.log(dateTimeNow.getDay());
-        switch (dateTimeNow.getDay()) {
+    verifyWeekend(dateTime) {
+        switch (dateTime.getDay()) {
             case 0:
-                var newNumberDate = "";
-                if (!this.configWorks.sunday) {
-                    newNumberDate = dateTimeNow.getDate() + 1;
-                } else {
-                    newNumberDate = dateTimeNow.getDate();
-                }
-
-                return this.setNewtimeDay(dateTimeNow, newNumberDate, hourResidual);
+                return new Date(dateTime.setDate(dateTime.getDate() + 1));
                 break;
+
             case 6:
-                var newNumberDate = "";
-                if (!this.configWorks.saturday) {
-                    newNumberDate = dateTimeNow.getDate() + 2;
-                } else {
-                    newNumberDate = dateTimeNow.getDate();
-                }
-                return this.setNewtimeDay(dateTimeNow, newNumberDate, hourResidual);
+                return new Date(dateTime.setDate(dateTime.getDate() + 2));
                 break;
             default:
-                return dateTimeNow;
+                return new Date(dateTime);
                 break;
         }
     }
 
-    setNewtimeDay(date, newNumberDate, hourResidual) {
-        date.setDate(newNumberDate);
-        date.setHours(this.configWorks.timeStart + hourResidual);
-        date.setMinutes(0);
-        date.setSeconds(0);
-        date.setMilliseconds(0);
-        return date;
-    }
+    verifyBeforeTimeStart(dateTime)
+    {
+        var intTimeStart = parseInt(this.config.timeStart);
+        var intDateTime = parseInt(dateTime.getHours());
+        
+        if(intDateTime < intTimeStart)
+        {
+            var intTimeResidualAfterLimit = 24 - parseInt(this.config.timeLimit);
+            var intDiffTimerBeforeTimeAfter = intTimeStart - intDateTime;
 
-    
+            var newIntTime = intDateTime + intDiffTimerBeforeTimeAfter + intTimeResidualAfterLimit;
 
+            if(this.config.lunch.lunchValue)
+            {
+                if(newIntTime > 12)
+                {
+                    newIntTime += 1;
+                }
+            }
+
+            return new Date(dateTime.setHours(newIntTime));
+        }
+
+        return dateTime;
+    }   
 }
+
 
 
 module.exports = Core;
